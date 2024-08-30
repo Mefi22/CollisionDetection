fc5adbd7d36b8ff6418bfe15c7a61d262938cca4
Mefi <shedgehog222@gmail.com>
2024-08-30 17:29:15 +0330

    Added Ray Detection

---
+++ src/main/java/imagetools/Toolbox.java
@@ -0,0 +1,20 @@
+package imagetools;
+
+import java.awt.*;
+import java.awt.image.BufferedImage;
+
+public class Toolbox {
+    private Toolbox() {}
+
+    public static BufferedImage getBufferedImage(Image img) {
+        BufferedImage bufferedImage = new BufferedImage(
+                img.getWidth(null),
+                img.getHeight(null),
+                BufferedImage.TYPE_INT_ARGB);
+        Graphics2D g2d = bufferedImage.createGraphics();
+        g2d.setRenderingHint(RenderingHints.KEY_RENDERING, RenderingHints.VALUE_RENDER_QUALITY);
+        g2d.drawImage(img, 0, 0, null);
+        g2d.dispose();
+        return bufferedImage;
+    }
+}
